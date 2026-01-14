93fa6a8c3f3e9419a6e527f335acc58495e2a74b
CosmosMount <yzhang1150@gmail.com>
2026-01-14 20:59:25 +0800

    [to fix] light effect

--- User/Service/Src/ServiceBooster.cpp
+++ User/Service/Src/ServiceBooster.cpp
@@ -12,7 +12,6 @@ uint8_t alive_thread_stack[512];
 
 extern TX_THREAD RemoterThread;
 extern TX_SEMAPHORE RemoterGot;
-extern TX_SEMAPHORE RemoterThreadSem;
 extern uint8_t RemoterThreadStack[1024];
 extern void RemoterThreadFun(ULONG initial_input);
 
@@ -52,32 +51,30 @@ UCHAR Msg_PoolBuf[4096] = {0};
     {
         /* Increment thread counter. */
         bool imu_alive = tx_semaphore_get(&IMUThreadSem, TX_NO_WAIT) == TX_SUCCESS;
-        bool remoter_alive = tx_semaphore_get(&RemoterThreadSem, 100) == TX_SUCCESS;
         bool referee_alive = tx_semaphore_get(&RefereeThreadSem, TX_NO_WAIT) == TX_SUCCESS;
         bool function_alive = tx_semaphore_get(&FunctionThreadSem, TX_NO_WAIT) == TX_SUCCESS;
         bool solver_alive = tx_semaphore_get(&SolverThreadSem, TX_NO_WAIT) == TX_SUCCESS;
         bool pendulum_alive = tx_semaphore_get(&PendulumThreadSem, TX_NO_WAIT) == TX_SUCCESS;
         bool ui_alive = tx_semaphore_get(&UIThreadSem, TX_NO_WAIT) == TX_SUCCESS;
-        if (referee_alive && function_alive && solver_alive && pendulum_alive && ui_alive)
+        if (imu_alive)
         {
-            if (imu_alive && remoter_alive)
+            if (solver_alive && function_alive && referee_alive && ui_alive)
             {
-                LED_blink(LED_COLOR::LED_GREEN);
-            }
-            else if (imu_alive)
-            {
-                LED_blink(LED_COLOR::LED_BLUE);
-            }
-            else if (remoter_alive)
-            {
-                LED_blink(LED_COLOR::LED_WHITE);
+                if (pendulum_alive)
+                {
+                    LED_blink(LED_COLOR::LED_GREEN);
+                }
+                else
+                {
+                    LED_blink(LED_COLOR::LED_BLUE);
+                }
             }
         }
         else 
         {
             LED_blink(LED_COLOR::LED_RED);
         }
-        tx_thread_sleep(500);
+        tx_thread_sleep(100);
     }
 }
 
@@ -123,5 +120,4 @@ extern "C" void ServiceBooster()
     tx_semaphore_create(&RemoterGot, TX_NAME("RemoterGot"), 0);
     tx_semaphore_create(&IMUThreadSem, TX_NAME("IMUThreadSem"), 0);
     tx_semaphore_create(&RefereeThreadSem, TX_NAME("RefereeThreadSem"), 0);
-    tx_semaphore_create(&RemoterThreadSem, TX_NAME("RemoterThreadSem"), 0);
 }
