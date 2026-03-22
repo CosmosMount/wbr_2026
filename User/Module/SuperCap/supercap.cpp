c1090eccf5753bf21e154bb9920d3e1450610f4b
CosmosMount <yzhang1150@gmail.com>
2026-03-22 14:14:38 +0800

    [to test] super cap

--- User/Module/SuperCap/supercap.cpp
+++ User/Module/SuperCap/supercap.cpp
@@ -1,5 +1,63 @@
-//
-// Created by cosmosmount on 2025/8/30.
-//
-
 #include "supercap.hpp"
+#include "bsp_can.hpp"
+#include "fdcan.h"
+
+void SuperCap::Init()
+{
+    supercap_set.set.cap_state_set = SuperCapON;
+    supercap_set.set.fly_extra_set = 0;
+    supercap_set.set.power_limit_set = 45;
+    supercap_set.set.raw_power_limit = 0;
+    supercap_set.set.remained_energy = 0;
+
+    supercap_fdb.fdb.cap_state_fdb = 0;
+    supercap_fdb.fdb.cap_voltage_x5 = 0;
+    supercap_fdb.fdb.input_power_x100 = 0;
+    supercap_fdb.fdb.cap_power_x100 = 0;
+    supercap_fdb.fdb.remained_energy = 0;
+}
+
+void SuperCap::SetCapState(SuperCapState state)
+{
+    supercap_set.set.cap_state_set = state;
+}
+
+void SuperCap::SetFlyExtra(uint8_t extra)
+{
+    supercap_set.set.fly_extra_set = extra;
+}
+
+void SuperCap::SetPowerLimit(float power)
+{
+    supercap_set.set.power_limit_set = power;
+}
+
+void SuperCap::SendCapData()
+{
+    CAN_Transmit(&hfdcan3, 0xC5, supercap_set._data, 8);
+}
+
+SuperCap::SuperCapState SuperCap::GetCapState()
+{
+    return static_cast<SuperCapState>(supercap_fdb.fdb.cap_state_fdb);
+}
+
+float SuperCap::GetCapVoltage()
+{
+    return supercap_fdb.fdb.cap_voltage_x5 * 0.2f;
+}
+
+float SuperCap::GetInputPower()
+{
+    return supercap_fdb.fdb.input_power_x100 * 0.01f;
+}
+
+float SuperCap::GetCapPower()
+{
+    return supercap_fdb.fdb.cap_power_x100 * 0.01f;
+}
+
+float SuperCap::GetCapEnergy()
+{
+    return supercap_fdb.fdb.cap_voltage_x5*supercap_fdb.fdb.cap_voltage_x5*0.1111111111f;
+}
--- User/Module/SuperCap/supercap.hpp
+++ User/Module/SuperCap/supercap.hpp
@@ -1,8 +1,63 @@
-//
-// Created by cosmosmount on 2025/8/30.
-//
+#pragma once
 
-#ifndef RM26_SUPERCAP_HPP
-#define RM26_SUPERCAP_HPP
+#include "main.h"
 
-#endif //RM26_SUPERCAP_HPP
+#include "bsp_can.hpp"
+
+struct supercap_set_t
+{
+    uint8_t cap_state_set;
+    uint8_t fly_extra_set;
+    float power_limit_set;
+    uint8_t raw_power_limit;
+    uint8_t remained_energy;
+} __attribute__((packed));
+
+struct supercap_fdb_t
+{
+    uint8_t cap_state_fdb;
+    uint8_t cap_voltage_x5;
+    int16_t input_power_x100;
+    int16_t cap_power_x100;
+    int16_t remained_energy;
+} __attribute__((packed));
+
+union supercap_set_u
+{
+    supercap_set_t set;
+    uint8_t _data[sizeof(supercap_set_t)];
+};
+
+union supercap_fdb_u
+{
+    supercap_fdb_t fdb;
+    uint8_t _data[sizeof(supercap_fdb_t)];
+};
+
+class SuperCap
+{
+public:
+    supercap_set_u supercap_set; ///< 用于发送给超级电容的数据
+    supercap_fdb_u supercap_fdb; ///< 用于接收超级电容的数据
+
+    typedef enum : uint8_t
+    {
+        SuperCapOFF = 0,
+        SuperCapON = 1,
+    } SuperCapState;
+
+    void Init();
+    // 设置超级电容的状态
+    void SetCapState(SuperCapState state);
+    void SetFlyExtra(uint8_t extra);
+    void SetPowerLimit(float power);
+    // 获取超级电容的状态
+    SuperCapState GetCapState();
+    float GetCapVoltage();
+    float GetInputPower();
+    float GetCapPower();
+    float GetCapInPower();
+    float GetCapEnergy();
+    void SendCapData();
+    void ReceiveCapData();
+};
